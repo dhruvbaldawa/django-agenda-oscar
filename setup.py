2aa5d9aab21acdfc365e7232a3918eb5e8dc78a8
Alan Trick <me@alantrick.ca>
2018-04-01 23:05:08 -0700

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='django-agenda',
-    version='0.3.4',
+    version='0.3.5',
     description='A scheduling app for Django.',
     long_description=read('README.rst'),
     author='Alan Trick',
